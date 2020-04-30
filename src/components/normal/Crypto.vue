121d2543d978414bb9e059be6a3ba9c618ab92cf
plaifa <59010823@kmitl.ac.th>
2020-04-30 21:20:07 +0700

    edit style

--- src/components/normal/Crypto.vue
+++ src/components/normal/Crypto.vue
@@ -1,33 +1,175 @@
 <template>
 	<div class="container-normal">
         <div class="row">
-            <div class="col">
-                    <h4>Encode</h4>
-                        <form>
-                            <div class="form-group">
-                                <form>
-                                   #somthing
-                                </form>
-                                <br>
-                                <h6>Enter your message:</h6>
-                                    <div class="form-group">
-                                        <textarea class="form-control" rows="2" id="comment"></textarea>
-                                    </div>
-                            </div>
-                        </form>
-                </div>
-                <div class="col">
-                    <h4>Decode</h4>
-                        <form>
-                            <div class="form-group">
-                                <br>
-                                <div class="form-group">
-                                    <form>
-                                      #somthing
-                                    </form>
+            <div class="col"  align="left">
+                    <h4>Encryption</h4>
+                    <div class="btn-group">
+                        <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Choose
+                        </button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item active" href="#">AES</a>
+                            <a class="dropdown-item" href="#">DES</a>
+                        </div>
+                    </div>
+                    <form>
+                        <div class="form-group">
+                            <h6>Select Input:</h6>
+                                 <div class="btn-group form-group">
+                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Choose
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item active" href="#">file</a>
+                                                <a class="dropdown-item" href="#">text</a>
+                                            </div>
                                 </div>
-                            </div>
-                        </form>	
+                        </div>
+                    </form>
+                     <form>
+                        <div class="form-group">
+                            <h6>Select Mode:</h6>
+                                 <div class="form-group">
+                                         <div class="btn-group">
+                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Choose
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item active" href="#">CBC</a>
+                                                <a class="dropdown-item" href="#">DES</a>
+                                            </div>
+                                        </div>
+                                </div>
+                        </div>
+                    </form>
+                     <form>
+                        <div class="form-group">
+                            <h6>Key Size:</h6>
+                                 <div class="form-group">
+                                         <div class="btn-group">
+                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Choose
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item active" href="#">1024</a>
+                                                <a class="dropdown-item" href="#">DES</a>
+                                            </div>
+                                        </div>
+                                </div>
+                        </div>
+                    </form>
+                    <form>
+                        <div class="form-group">
+                            <h6>Enter Secret key:</h6>
+                                 <div class="form-group">
+                                        <textarea class="form-control" rows="1" id="comment"></textarea>
+                                </div>
+                        </div>
+                    </form>
+                    <form>
+                        <div class="form-group">
+                            <h6>Output format :</h6>
+                                 <div class="form-group">
+                                         <div class="btn-group">
+                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Choose
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item active" href="#">HEX</a>
+                                                <a class="dropdown-item" href="#">DEC</a>
+                                                <a class="dropdown-item" href="#">BIN</a>
+                                            </div>
+                                        </div>
+                                </div>
+                        </div>
+                    </form>
+
+                </div>
+                <div class="col"  align="left">
+                    <h4>Decryption</h4>
+                    <div class="btn-group">
+                        <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Choose
+                        </button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item active" href="#">AES</a>
+                            <a class="dropdown-item" href="#">DES</a>
+                        </div>
+                    </div>
+                    <form>
+                        <div class="form-group">
+                            <h6>Select Input:</h6>
+                                 <div class="form-group">
+                                         <div class="btn-group">
+                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Choose
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item active" href="#">AES</a>
+                                                <a class="dropdown-item" href="#">DES</a>
+                                            </div>
+                                        </div>
+                                </div>
+                        </div>
+                    </form>
+                     <form>
+                        <div class="form-group">
+                            <h6>Select Mode:</h6>
+                                 <div class="form-group">
+                                         <div class="btn-group">
+                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Choose
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item active" href="#">AES</a>
+                                                <a class="dropdown-item" href="#">DES</a>
+                                            </div>
+                                        </div>
+                                </div>
+                        </div>
+                    </form>
+                     <form>
+                        <div class="form-group">
+                            <h6>Key Size:</h6>
+                                 <div class="form-group">
+                                         <div class="btn-group">
+                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Choose
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item active" href="#">AES</a>
+                                                <a class="dropdown-item" href="#">DES</a>
+                                            </div>
+                                        </div>
+                                </div>
+                        </div>
+                    </form>
+                    <form>
+                        <div class="form-group">
+                            <h6>Enter Secret key:</h6>
+                                 <div class="form-group">
+                                        <textarea class="form-control" rows="1" id="comment"></textarea>
+                                </div>
+                        </div>
+                    </form>
+                    <form>
+                        <div class="form-group">
+                            <h6>Output format :</h6>
+                                 <div class="form-group">
+                                         <div class="btn-group">
+                                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Choose
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item active" href="#">HEX</a>
+                                                <a class="dropdown-item" href="#">DEC</a>
+                                                <a class="dropdown-item" href="#">BIN</a>
+                                            </div>
+                                        </div>
+                                </div>
+                        </div>
+                    </form>
+
                 </div>
             </div>
             <div class="row">
@@ -35,14 +177,14 @@
             </div>
             <div class="col-sm-1">
                 <button class="btn btn-primary">
-                    <a href="/Encrypt" class="nav-link">Encode</a>
+                    <a href="/Encrypt" class="nav-link">Encryption</a>
                 </button>
             </div>
             <div class="col-sm-5">
             </div>
             <div class="col-sm-1">
-                <button class="btn btn-primary">
-                    <a href="/Decrypt" class="nav-link">Decode</a>
+                <button class="btn btn-danger">
+                    <a href="/Decrypt" class="nav-link">Decryption</a>
                 </button>
             </div>
         </div>
@@ -53,15 +195,16 @@
 
 <style>
     .col {
-        background-color: #85b3cc;
+        background-color: #ffff;
         border: 1px solid gray;
         margin: 40px;
-        height: 250px;
+        height: 600;
         padding: 30px;
     }
 
     .container {
-        text-align: center;
+        text-align: left;
+        
     }
 
     button {
